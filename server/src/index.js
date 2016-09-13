c384c79a11ffb75e156ed25bd167c639586cb3a9
frankNowinski <nowinski.frank@gmail.com>
2016-09-13 19:57:17 -0400

    added client side validations to see if stock exists

--- server/src/index.js
+++ server/src/index.js
@@ -11,28 +11,9 @@ let app = express();
 
 app.use(bodyParser.json());
 
-app.use('/query', query);
+// app.use('/query', query);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/stocks', stocks);
 
 app.listen(8080, () => console.log('Listening on port 8080'));
-
-// app.use(function (req, res, next) {
-//
-//   // Website you wish to allow to connect
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-//
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//
-//   // Pass to next layer of middleware
-//   next();
-// });
